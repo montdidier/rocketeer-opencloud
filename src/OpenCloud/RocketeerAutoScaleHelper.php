bb36d591a27d328865f380a4f23a13645d2b6c09
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2014-12-18 10:01:10 +0800

    moved autoscale group argument from global to addresses call

--- src/OpenCloud/RocketeerAutoScaleHelper.php
+++ src/OpenCloud/RocketeerAutoScaleHelper.php
@@ -18,23 +18,23 @@ class RocketeerAutoScaleHelper
   public function __construct()
   {
     $this->config = RocketeerOpenCloudConfiguration::instantiate();
-    $this->client =
-      new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
-                    'username' => $this->config->username,
-                    'apiKey' => $this->config->api_key
-                  ));
+    $this->client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT,
+      array(
+        'username' => $this->config->username,
+        'apiKey' => $this->config->api_key
+    ));
     $this->autoscaleService = $this->client->autoscaleService(null, $this->config->region);
     $this->computeService = $this->client->computeService(null, $this->config->region);
   }
 
-  public function addresses($username, $agent)
+  public function addresses($groupName, $username, $agent)
   {
     $groupList = $this->autoscaleService->groupList();
 
     $addresses = array();
     while($group = $groupList->next()) {
       foreach($group->getState()->active as $active) {
-        if ($group->getState()->name == $this->config->autoscale_group) {
+        if ($group->getState()->name == $groupName) {
           $server = $this->computeService->server($active->id);
           array_push($addresses, array(
             'host' => $server->addresses->private[0]->addr,
--- src/OpenCloud/RocketeerOpenCloudConfiguration.php
+++ src/OpenCloud/RocketeerOpenCloudConfiguration.php
@@ -1,39 +1,49 @@
 <?php
 namespace Rocketeer\Plugins\OpenCloud;
 
-class ConfigInstance
+class ConfigurationInstance
 {
+  /**
+   */
   public $username;
-  public $api_key;
-  public $region;
-  public $autoscale_group;
 
   /**
    */
-  public function __construct($username, $api_key, $region, $autoscale_group)
+  public $api_key;
+
+  /**
+   */
+  public $region;
+
+  /**
+   */
+  public function __construct($username, $api_key, $region)
   {
     $this->username = $username;
     $this->api_key = $api_key;
     $this->region = $region;
-    $this->autoscale_group = $autoscale_group;
   }
 }
 
 class RocketeerOpenCloudConfiguration
 {
+  /**
+   */
   static public $username;
 
+  /**
+   */
   static public $api_key;
 
+  /**
+   */
   static public $region;
-
-  static public $autoscale_group;
 
   /**
    */
   static public function instantiate()
   {
-    return new ConfigInstance(self::$username, self::$api_key, self::$region, self::$autoscale_group);
+    return new ConfigurationInstance(self::$username, self::$api_key, self::$region);
   }
 }
 
